5fef62b447896310bbe06b144bf6d153dd818d41
Philip <pbazzard@gmail.com>
2017-03-13 19:40:39 +0000

    printed buffering

--- printBuffString.js
+++ printBuffString.js
@@ -5,15 +5,18 @@ var requestOptions = {
     path: '/http-examples/step1.html'
   };
 
-function getAndPrintHTMLChunks () {
+function getAndPrintHTML () {
 
   https.get(requestOptions, function (response) {
   // set encoding of received data to UTF-8
     response.setEncoding('utf8');
 
+    var buff = '';
+
     // the callback is invoked when a `data` chunk is received
     response.on('data', function (data) {
-      console.log(buff + "\n");
+      buff += data;
+      console.log(buff + "/n");
     });
 
     // the callback is invoked when all of the data has been received
@@ -26,4 +29,4 @@ function getAndPrintHTMLChunks () {
 
 };
 
-getAndPrintHTMLChunks();
+getAndPrintHTML();
